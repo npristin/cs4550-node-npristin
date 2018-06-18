0dc8c1f06a1a5711a40aab5f6600dce393bd1baa
nicolepristin <nicole.pristin@gmail.com>
2018-06-17 23:43:27 -0400

    Update section model

---
+++ models/section/section.model.server.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+var sectionSchema = require('./section.schema.server');
+var sectionModel = mongoose.model('SectionModel', sectionSchema);
+
+function createSection(section) {
+  return sectionModel.create(section);
+}
+
+function findSectionsForCourse(courseId) {
+  return sectionModel.find({courseId: courseId});
+}
+
+function decrementSectionSeats(sectionId) {
+  return sectionModel.update({
+    _id: sectionId
+  }, {
+    $inc: {seats: -1}
+  });
+}
+
+function incrementSectionSeats(sectionId) {
+  return sectionModel.update({
+    _id: sectionId
+  }, {
+    $inc: {seats: +1}
+  });
+}
+
+function deleteSection(sectionId) {
+    return sectionModel.remove({_id: sectionId});
+}
+
+function findSectionById(sectionId) {
+    var section = sectionModel.findById(sectionId);
+    return section;
+}
+
+function updateSection(section, sectionId) {
+    return sectionModel.update({_id: sectionId}, {$set: section});
+}
+
+module.exports = {
+  createSection: createSection,
+  findSectionsForCourse: findSectionsForCourse,
+  decrementSectionSeats: decrementSectionSeats,
+  incrementSectionSeats: incrementSectionSeats,
+  deleteSection: deleteSection,
+  findSectionById: findSectionById,
+  updateSection: updateSection
+};
